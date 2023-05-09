78eb4c438a5f1667994bc56f824b289082396355
konawave <nickkjeffs@gmail.com>
2023-05-09 14:38:23 -0400

    Added pseudocode for JS.

---
+++ Dev/script.js
@@ -0,0 +1,9 @@
+// Weather Dashboard with form inputs
+    // When I input a city I am presented with current and future conditions for that city and that city is added to search history (local storage?).
+    // Validate entry. If not a valid city, alert that entry not valid.
+// When I look up a city, I see City name, current date, and an icon representation of weather (API), temperature, humidity, windspeed. All in provided weather API.
+    // If input === city, make entry visible.
+// When I look at future dates, then I can see up to 5 days in advance that display all of the above info.
+    // should display in a grid
+// I need to be able to click on a city in search history and go back to that entry. 
+    // This should be done with local history - maybe display key, then retrieve if entry clicked on? 
