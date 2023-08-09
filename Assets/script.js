1d8dbda0e7fb9540359d4d90ec93caff2f1540ef
konawave <nickkjeffs@gmail.com>
2023-08-09 15:02:32 -0400

    Pausing progress on this to resume working on tech blog

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,15 @@ const inputBar = document.querySelector('.city')
 const searchBtn = document.querySelector('.btn btn-primary search')
 const clearBtn = document.querySelector(".btn btn-danger clear")
 
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?";
+
 
 // API calls - The call is made when the search button is selected
 
 // handler function - when the search button is clicked, make the API call, 
+    // populates the City box with information on that city today
+    // Populates 5-day forecast boxes
+
+function searchHandler() {
+
+}
